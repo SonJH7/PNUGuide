bf7ba48a8c762ad0922f56b81db3f4597b74dec9
이지원 <superjiwon11@gmail.com>
2025-06-09 16:07:27 +0900

    홈화면 설정

--- app/src/main/java/com/pnu/pnuguide/ui/SettingsActivity.kt
+++ app/src/main/java/com/pnu/pnuguide/ui/SettingsActivity.kt
@@ -2,11 +2,16 @@ package com.pnu.pnuguide.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 import com.pnu.pnuguide.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar_settings)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener { finish() }
     }
 }
