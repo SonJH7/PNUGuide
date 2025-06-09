00506745e9afb2392097772a6174d286b0438e7c
이지원 <superjiwon11@gmail.com>
2025-06-09 15:51:44 +0900

    상단 버튼 수정

--- app/src/main/java/com/pnu/pnuguide/MainActivity.kt
+++ app/src/main/java/com/pnu/pnuguide/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.fragment.app.commit
 import com.pnu.pnuguide.ui.chat.ChatFragment
@@ -14,15 +15,19 @@ import com.pnu.pnuguide.ui.stamp.StampFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
                 setContentView(R.layout.activity_main)
 
         drawerLayout = findViewById(R.id.drawer_layout)
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.app_name, R.string.app_name)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
 
         findViewById<NavigationView>(R.id.navigation_view).setNavigationItemSelectedListener { item ->
             when (item.itemId) {
@@ -47,9 +52,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-        private fun switchFragment(fragment: androidx.fragment.app.Fragment) {
+            private fun switchFragment(fragment: androidx.fragment.app.Fragment) {
         supportFragmentManager.commit {
             replace(R.id.fragment_container, fragment)
         }
+    }
+
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
