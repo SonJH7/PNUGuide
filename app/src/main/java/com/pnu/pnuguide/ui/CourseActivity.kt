3942840396f1575fd1e40973683010ae436fca96
Jiwon Lee <148948672+2-z-won@users.noreply.github.com>
2025-06-09 17:33:16 +0900

    feat: open screens and enlarge bottom nav

---
+++ app/src/main/java/com/pnu/pnuguide/ui/CourseActivity.kt
@@ -0,0 +1,12 @@
+package com.pnu.pnuguide.ui.course
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pnu.pnuguide.R
+
+class CourseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_course)
+    }
+}
---
+++ app/src/main/java/com/pnu/pnuguide/ui/StampActivity.kt
@@ -0,0 +1,12 @@
+package com.pnu.pnuguide.ui.stamp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pnu.pnuguide.R
+
+class StampActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_stamp)
+    }
+}
