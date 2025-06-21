e24e333def84489aae2904808308dfc25044221c
Jiwon Lee <148948672+2-z-won@users.noreply.github.com>
2025-06-22 01:14:19 +0900

    Add bottom nav styling and profile edit

---
+++ app/src/main/java/com/pnu/pnuguide/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.pnu.pnuguide.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pnu.pnuguide.R
+
+/**
+ * Simple activity that allows the user to edit profile information.
+ */
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
