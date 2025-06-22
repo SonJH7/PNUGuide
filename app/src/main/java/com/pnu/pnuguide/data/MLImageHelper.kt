82307ee8e1a63ce0ccf7ae3df3597eb6dbe617a0
Jiwon Lee <148948672+2-z-won@users.noreply.github.com>
2025-06-22 21:33:32 +0900

    Add camera capture button and bitmap preprocessing

--- app/src/main/java/com/pnu/pnuguide/data/MLImageHelper.kt
+++ app/src/main/java/com/pnu/pnuguide/data/MLImageHelper.kt
@@ -1,10 +1,30 @@
 package com.pnu.pnuguide.data
 
 import android.graphics.Bitmap
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import org.tensorflow.lite.support.image.processor.ImageProcessor
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.NormalizeOp
 
 object MLImageHelper {
+    fun bitmapToTensorImage(bitmap: Bitmap): TensorImage {
+        val processor = ImageProcessor.Builder()
+            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+            .add(NormalizeOp(0f, 255f))
+            .build()
+        val tensor = TensorImage(DataType.FLOAT32)
+        tensor.load(bitmap)
+        return processor.process(tensor)
+    }
+
+    fun bitmapToTensorBuffer(bitmap: Bitmap): TensorBuffer {
+        return bitmapToTensorImage(bitmap).tensorBuffer
+    }
     suspend fun matchSpot(bitmap: Bitmap): String? {
         // TODO: replace this placeholder logic with TensorFlow Lite model inference
         return if (bitmap.width > 0 && bitmap.height > 0) "dummy_spot" else null
     }
 }
+
