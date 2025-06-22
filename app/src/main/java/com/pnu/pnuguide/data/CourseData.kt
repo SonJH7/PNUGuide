57ec38f48694583210c4b973206b63d1815f1d6c
Jiwon Lee <148948672+2-z-won@users.noreply.github.com>
2025-06-23 03:20:42 +0900

    Use label codes as stamp ids

--- app/src/main/java/com/pnu/pnuguide/data/CourseData.kt
+++ app/src/main/java/com/pnu/pnuguide/data/CourseData.kt
@@ -2,8 +2,8 @@ package com.pnu.pnuguide.data
 
 object CourseData {
     fun loadStamps(): List<Stamp> {
-        return List(24) { index ->
-            Stamp(id = "spot${index + 1}", name = "Spot ${index + 1}")
+        return LabelMappings.labelToTitle.map { (code, title) ->
+            Stamp(id = code, name = title)
         }
     }
 }
